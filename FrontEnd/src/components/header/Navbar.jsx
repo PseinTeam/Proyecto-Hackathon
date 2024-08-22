cf8c8602b33463ad280ba8e291e4ed1a0314c6ee
[Tomas Valdez] <Tomasvaldez02@gmail.com>
2024-08-22 11:20:40 -0300

    style: :art: Cambios en Navbar.jsx, integracion de un logo, y modificacion a index.html
    Se realizaron cambios de nombre de la navegacion del navbar, ademas de agregar un logo tanto en el navbar como en el index.html

--- FrontEnd/src/components/header/Navbar.jsx
+++ FrontEnd/src/components/header/Navbar.jsx
@@ -1,32 +1,33 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../../../public/img/logo.png";
 
 export const Navbar = () => {
   return (
     <div>
       <div className="container">
         <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-          <div className="col-md-3 mb-2 mb-md-0">
+          <div className="col-md-2 mb-2 mb-md-0">
             <a
               href="/"
               className="d-inline-flex link-body-emphasis text-decoration-none"
             >
-              <svg
+              <img
+                src={logo}
                 className="bi"
                 width="40"
-                height="32"
+                height="37"
                 role="img"
-                aria-label="Bootstrap"
-              >
-                <use xlinkHref="#bootstrap"></use>
-              </svg>
+                aria-label="Logo"
+                alt=""
+              />
             </a>
           </div>
 
           <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
             <li>
               <a href="/" className="nav-link px-2 link-secondary">
-                Home
+                Inicio
               </a>
             </li>
             <li>
@@ -41,12 +42,12 @@ export const Navbar = () => {
             </li>
             <li>
               <a href="#" className="nav-link px-2">
-                FAQs
+                Reconocimiento de ambiente
               </a>
             </li>
             <li>
               <a href="#" className="nav-link px-2">
-                About
+                Asistente virtual
               </a>
             </li>
           </ul>
