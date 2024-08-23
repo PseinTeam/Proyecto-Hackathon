1cc17c0d9c4c57db012e93d20c9bc14315cd0db2
Milanesa 21 <diegojara88574@gmail.com>
2024-08-23 12:24:58 -0300

    fix: Se arreglaron los errores en las importaciones y ahora andan las funcionalidades

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import init_db
 from services.roles_permisos_asignacion import Db_insert_RP
 from routes.socket_routes import router as websocket_router
+from routes.ia_routes import router as ia_router
+from routes.ia_ambiente_routes import router as ia_ambiente_router
 
 
 # Para iniciar el proyecto: uvicorn main:app --reload
@@ -32,6 +34,8 @@ app.include_router(alert_router)
 app.include_router(user_rutes)
 app.include_router(auth_router)
 app.include_router(permiso_rutes)
+app.include_router(ia_router)
+app.include_router(ia_ambiente_router)
 
 
 Db_insert_RP()
