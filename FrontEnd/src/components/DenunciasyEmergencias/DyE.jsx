4e9f351cb53b849dee07f5177e76777b3eb0056d
IvanPz01 <ivanciito_999@hotmail.com>
2024-08-23 13:18:29 -0300

    cambios a la logica de emergencias}

--- FrontEnd/src/components/DenunciasyEmergencias/DyE.jsx
+++ FrontEnd/src/components/DenunciasyEmergencias/DyE.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import "/public/css/components/inputtext.css";
 import "/public/css/components/button.css";
+import { AuthContext } from "../../context/AuthProvider";
+import { ca } from "date-fns/locale";
 
 export const DenunciasyEmergencias = () => {
+  const [denunciaMessage, setDenunciaMessage] = useState("");
+  const [notification, setNotification] = useState({ open: false, severity: '', message: '' });
+
+  const { user } = useContext(AuthContext);
+
+  const handleDenuncia = async (message) => {
+    try {
+      const response = await fetch("http://localhost:8000/SendAlertMessage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: user.id,
+          full_name: user.full_name,
+          puesto_trabajo: user.puesto_trabajo,
+          message: message,
+        }),
+      });
+
+      if (response.ok) {
+        setNotification({ open: true, severity: 'success', message: 'Mensaje enviado correctamente' });
+      } else {
+        const data = await response.json();
+        console.log("Error al enviar el mensaje:", data.detail);
+        setNotification({ open: true, severity: 'error', message: 'Error al enviar mensaje' });
+      }
+    }catch(error){
+      console.log("Error al enviar el mensaje:", error);
+      setNotification({ open: true, severity: 'error', message: 'Error al enviar mensaje' });
+    }
+  };
+
+  const handleDenunciaChange = (e) => {
+    setDenunciaMessage(e.target.value);
+  };
+
+  const handleCloseNotification = () => {
+    setNotification({ ...notification, open: false });
+  };
+
+
   return (
     <div className="SECCION">
       <section id="Denuncias">
--- backend/database/db.py
+++ backend/database/db.py
@@ -3,14 +3,17 @@ from sqlalchemy.engine import URL
 from config import DB_DRIVER, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 from sqlalchemy.orm import sessionmaker,declarative_base
 
+DB_USERNAME_TEST = 'ivanpz'
+DB_NAME_TEST = 'aña'
+
 # Crear la URL de conexión a la base de datos usando las variables de entorno
 url = URL.create(
     drivername=DB_DRIVER,
-    username=DB_USERNAME,
+    username=DB_USERNAME_TEST,
     password=DB_PASSWORD,
     host=DB_HOST,
     port=DB_PORT,
-    database=DB_NAME
+    database=DB_NAME_TEST
 )
 
 # Crear el motor de SQLAlchemy
