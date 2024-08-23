e8c4a6ad884cb5e23d1d8723d057a3e12d142566
[Tomas Valdez] <Tomasvaldez02@gmail.com>
2024-08-23 09:57:26 -0300

    feat: :sparkles: Se realizaron introducciiones de codigo para Notallowwed.jsx, Changepersonaldata.jjsx, Changepassword.jsx, Employeepanel.jsx
    Cambios de ventanas y pestañas para mas funcionalidades

--- FrontEnd/src/components/CerrarSesion.jsx
+++ FrontEnd/src/components/CerrarSesion.jsx
@@ -1,30 +1,30 @@
-import { useContext } from "react"
-import { AuthContext } from "../context/AuthProvider.jsx"
-import { useNavigate } from "react-router-dom"
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider.jsx";
+import { useNavigate } from "react-router-dom";
 
 export const CerrarSesion = () => {
-    const {logout, user} = useContext(AuthContext)
-    const navigate = useNavigate()
-    const cerrarSesion = () => {
-        console.log('Cerrando sesión')
-        logout()
-        navigate('/')
-        localStorage.removeItem('token')
-    }
+  const { logout, user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const cerrarSesion = () => {
+    console.log("Cerrando sesión");
+    logout();
+    navigate("/");
+    localStorage.removeItem("token");
+  };
   return (
     <div className="col-md-3 text-end">
-        <a style={{cursor: 'pointer'}} onClick={cerrarSesion}>
-            <button type="button" className="btn btn-primary">
-                Cerrar Sesión
-            </button>
-        </a>
-        {user?.rol?.nombre === "admin" && (
+      <a style={{ cursor: "pointer" }} onClick={cerrarSesion}>
+        <button type="button" className="btn btn-primary">
+          Cerrar Sesión
+        </button>
+      </a>
+      {user?.rol?.nombre === "admin" && (
         <a href="/Register" className="RegisterBtn">
-            <button type="button" className="btn btn-primary">
-                Registro
-            </button>
+          <button type="button" className="btn btn-primary ms-2">
+            Registro
+          </button>
         </a>
-        )}
+      )}
     </div>
-  )
-}
+  );
+};
